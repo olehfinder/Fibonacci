759f62485c1db5594f0afb0eb481da1926bc4fb1
Oleh Tymoshchuk <olegdigger777@gmail.com>
2018-10-28 22:03:38 +0000

    fib

---
+++ js/index.js
@@ -0,0 +1,11 @@
+alert("Hello");
+const num = parseFloat(prompt('Enter a number'));
+document.write = (arr);
+
+function fib(n){
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1])
+  }
+   return arr[n]
+}
