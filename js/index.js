4cec5720ee566bef6cb26447383fa8f207996d99
Oleh Tymoshchuk <olegdigger777@gmail.com>
2018-10-29 07:23:26 +0000

    Fibonacci

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 alert("Hello");
 const num = parseFloat(prompt('Enter a number'));
-document.write = (arr);
 
-function fib(n){
+function fib(num){
   let arr = [0, 1];
-  for (let i = 2; i < n + 1; i++){
+  for (let i = 2; i < num + 1; i++){
     arr.push(arr[i - 2] + arr[i -1])
   }
-   return arr[n]
+   return arr.pop();
 }
+
+alert(fib(num));
